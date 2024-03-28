5580b369881636da6873c86fe507d83972d32853
flo6669 <147403414+flo6669@users.noreply.github.com>
2024-03-28 18:55:16 +0100

    added detection and selection of USB-Stick

---
+++ src/Model/UsbDrive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effektive_Praesentationen.Model
+{
+    public class UsbDrive
+    {
+        public string? Name { get; set; }
+        public string? VolumeLabel { get; set; }
+        public string? DriveFormat { get; set; }
+        public long TotalSize { get; set; }
+        public long TotalFreeSpace { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}{VolumeLabel}  Freier Platz: {TotalFreeSpace} MB";
+        }
+    }
+}
--- src/ViewModel/FileSelectionViewModel.cs
+++ src/ViewModel/FileSelectionViewModel.cs
@@ -11,6 +11,9 @@ using System.Threading.Tasks;
 using System.Windows.Navigation;
 using Effektive_Praesentationen.Model;
 using System.Runtime.InteropServices;
+using System.Management;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace Effektive_Praesentationen.ViewModel
 {
@@ -24,7 +27,7 @@ namespace Effektive_Praesentationen.ViewModel
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(FileValid))]
         [NotifyCanExecuteChangedFor(nameof(NavigateToInactiveLoopCommand))]
-        private string test;
+        private string? test;
         //should we working without the "Test" variable, because it should reevalaute the FileValid property and CanExecute when changing the
         //value of Chapters.DefaultChapter
         //--> [NotifyPropertyChangedFor(nameof(FileValid))] and [NotifyCanExecuteChangedFor(nameof(NavigateToInactiveLoopCommand))]
@@ -33,11 +36,18 @@ namespace Effektive_Praesentationen.ViewModel
         [ObservableProperty]
         private INavigationService _navigation;
 
+        private ManagementEventWatcher? _insertionWatcher;
+        private ManagementEventWatcher? _removalWatcher;
+
+        public ObservableCollection<UsbDrive> UsbDrives { get; set; } = new ObservableCollection<UsbDrive>();
+
         public FileSelectionViewModel(INavigationService navService)
         {
             Chapters = new Model.Chapters();
             Navigation = navService;
             viewName = "FileSelection";
+            InitializeWatcher();
+            GetUsbInfo();
         }
 
         [RelayCommand(CanExecute = nameof(FileValid))]
@@ -84,6 +94,51 @@ namespace Effektive_Praesentationen.ViewModel
             Test = files[0];
         }
 
+        private async Task InitializeWatcher()  //watcher for usb stick
+        {
+            _insertionWatcher = new ManagementEventWatcher();
+            WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2");
+            _insertionWatcher.EventArrived += Watcher_EventArrived;
+            _insertionWatcher.Query = query;
+            _insertionWatcher.Start();
 
+            await Task.Run(() => _insertionWatcher.WaitForNextEvent());
+
+            _removalWatcher = new ManagementEventWatcher();
+            WqlEventQuery query2 = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 3");
+            _removalWatcher.EventArrived += Watcher_EventArrived;
+            _removalWatcher.Query = query2;
+            _removalWatcher.Start();
+
+            await Task.Run(() => _removalWatcher.WaitForNextEvent());
+        }
+
+        private void Watcher_EventArrived(object sender, EventArrivedEventArgs e) 
+        {
+            GetUsbInfo();
+        }
+
+        private void GetUsbInfo()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            Application.Current.Dispatcher.Invoke(() => UsbDrives.Clear());
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        UsbDrives.Add(new UsbDrive
+                        {
+                            Name = drive.Name,
+                            VolumeLabel = drive.VolumeLabel,
+                            DriveFormat = drive.DriveFormat,
+                            TotalSize = drive.TotalSize / 1000000,
+                            TotalFreeSpace = drive.AvailableFreeSpace / 1000000
+                        });
+                    });
+                } 
+            }
+        }
     }
 }
